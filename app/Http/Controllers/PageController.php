8eb5f4a8ab81afa7026dbd9859c18290a37e8643
sandhiyasree <sandhiya413@gmail.com>
2018-03-24 12:45:24 +0530

    Session and expire time checking

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Session;
+
+class PageController extends Controller{
+
+    public function getIndex()
+    {
+        // $session_key    =   rand(000000,999999);
+        // Session::put('mykey_'.$session_key,$session_key);
+        // $mykey          =   Session::get('mykey_'.$session_key);
+        // var_dump (session()->all());
+        return view('page.index',['title' => $mykey]);
+    }
+    
+}
+
+?>
